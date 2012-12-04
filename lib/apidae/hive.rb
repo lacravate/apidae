01ebe4eb53212a3ed0de537536d3e51e24f55179
lacravate <lacravate@lacravate.fr>
2012-12-04 02:10:41 +0100

    Sinatra application backbone

---
+++ lib/apidae/hive.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+# Libraries API best bud !
+require 'forwardable'
+
+module Apidae
+
+  # Do i need more ? We'll see.
+  class HiveImplationException < Exception; end
+
+  class Hive < Sinatra::Base
+
+    # Libraries API best bud !
+    # i am old but i haven't reached dotage yet.
+    # I just like to repeat mylself
+    extend Forwardable
+
+    # less code
+    # and "settings" is not pretty. So...
+    def_delegators :settings, :location, :population
+
+    # routes from configuration, registering and calling
+    register Sinatra::WaysAndMeans
+    ways_and_means!
+
+    # clumsy and tasteless, i must change that
+    if respond_to? :location
+      # we'll browse filesystem from this directory
+      # set as a Pathname
+      self.location = Pathname(location).realpath
+    else
+      raise HiveImplationError, "Sorry, dude, the hive can be implanted on nil (set location to anything in conf')"
+    end
+
+    # Who's populating the hive ?
+    if respond_to? :population
+      # insects defined in the configuration ?
+      require "apidae/#{population}"
+    else
+      # or the basic generic apidae described in the lib' ?
+      Construct, Queen, Population = Apidae::Construct, Apidae::Queen, Apidae::Worker
+    end
+
+    # Now we know who's who, where's where and what's what,
+    # define the application API and behaviors
+    include Construct
+    include Queen
+    include Population
+
+  end
+
+end
