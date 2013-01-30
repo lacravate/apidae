d792b4003dc2aa8cd48d0ff4e6923f525b4b7a07
lacravate <david.elbaz@af83.com>
2013-01-30 02:47:05 +0100

    Wax; does rendering only through String-derived classes

--- lib/apidae/wax.rb
+++ lib/apidae/wax.rb
@@ -1,16 +1,6 @@
 # encoding: utf-8
 
-# i love to make people laugh. Myself to begin with, but everyone
-# I can't do anything against that. So here we go...
-
-# Raw constructs turned into html elements
-
-# It all started because i did not want to make use of any templating
-# system for this minimal application, and because i also didn't want at
-# all to bother about template files in the project.
-
-# I only wanted to be able to handle this part of the application with a
-# neat Ruby 'require'
+require 'base64'
 
 module Apidae
 
@@ -18,84 +8,45 @@ module Apidae
   # forcibly, if not at all, by the other apidae. But it sounded cooler
   # and shorter
   module Wax
+    MOLDS = {
+      Assemblage: [
+        "PGh0bWw+CiAgPGhlYWQ+CiAgICA8dGl0bGU+CiAgICAgIEFwaWRhZSBIaXZl\nIC0gCiAgICA8L3RpdGxlPgogIDwvaGVhZD4KICA8Ym9keT4KCiAgPC9ib2R5\nPgo8L2h0bWw+Cg==\n",
+        { body: /(body>)(.*?)(<\/body)/m, title: /(title>)(.*?)(<\/title)/m, head: /(head>)(.*?)(<\/head)/m }
+      ],
+      Style:      [ "PHN0eWxlPgpib2R5e21hcmdpbjowOyBwYWRkaW5nOjA7IGZvbnQtc2l6ZTox\nM3B4OyBmb250LWZhbWlseTpHZW9yZ2lhLCAiVGltZXMgTmV3IFJvbWFuIiwg\nVGltZXMsIHNlcmlmOyBjb2xvcjojNjY2NjY2OyBiYWNrZ3JvdW5kLWNvbG9y\nOiNGRkZGRkY7IH0KaW1ne2Rpc3BsYXk6YmxvY2s7IG1hcmdpbjowOyBwYWRk\naW5nOjA7IGJvcmRlcjpub25lO30KYXtvdXRsaW5lOm5vbmU7IHRleHQtZGVj\nb3JhdGlvbjpub25lOyBjb2xvcjojMDU5QkQ4OyBiYWNrZ3JvdW5kLWNvbG9y\nOiNGRkZGRkY7fQpsaXtsaXN0LXN0eWxlLXR5cGU6bm9uZTsgbWFyZ2luOjA7\nIHBhZGRpbmc6MDt9CnVseyBwYWRkaW5nLWxlZnQ6IDIwcHg7fQo8L3N0eWxl\nPgo=\n" ],
+      Link:       [ "PGEgaHJlZj0iLy8iPjwvYT4=\n",     [ /(\/)(.*?)(\/)/, /(\/)(.*?)(\")/, /(>)(.*?)(<)/ ] ],
+      Image:      [ "PGltZyBzcmM9Ii9tZWRpYS8iPg==\n", /(\/)(.*?)(\")/ ],
+      Text:       [ "PHA+PC9wPg==\n",                 /(>)(.*?)(<)/ ],
+      Formated:   [ "PHByZT48L3ByZT4=\n",             /(>)(.*?)(<)/ ],
+      List:       [ "PHVsPjwvdWw+\n",                 /(ul>)(.*?)(<\/ul)/ ],
+      ListItem:   [ "PGxpPjwvbGk+\n",                 /(>)(.*?)(<)/ ]
+    }.freeze
 
-    # so here, one method per HTML element.
-    # Template element is a base64-encoded string
-    # decoded, on which substitutions are made
-    # Giggles !
-    class << self
-
-      # img src = ...
-      def image(source)
-        (@image ||= Base64.
-          decode64("PGltZyBzcmM9Ii9tZWRpYS8iPg==\n")).
-          gsub '/"', "/#{source}\""
-      end
-
-      # a href = /action/path , text of the link
-      def link(action, path, text)
-        (@link ||= Base64.
-          decode64("PGEgaHJlZj0iLy8iPjwvYT4=\n")).
-          gsub('//', "/#{action}/").
-          gsub('/"', "/#{path}\"").
-          gsub('><', ">#{text}<")
-      end
-
-      # p
-      def text(text)
-        (@text ||= Base64.
-          decode64("PHA+PC9wPg==\n")).
-          gsub '><', ">#{text}<"
-      end
-
-      # pre
-      def formated(text)
-        (@formated ||= Base64.
-          decode64("PHByZT48L3ByZT4=\n")).
-          gsub '><', ">#{text}<"
-      end
-
-      # the most ludicrously rendered
-      # HTML line-break in the world, na ?
-      def new_line
-        @new_line ||= Base64.
-          decode64("PGJyLz4=\n")
-      end
-
-      # try to display file contents in HTML page
-      def content(path)
-        # img tag
-        if path.mime.start_with?('image')
-          image path
-        # pre section with file contents
-        elsif path.mime.start_with?('text')
-          formated path.read
-        # link to download
-        else
-          link 'media', path, "Download #{path.basename} here"
+    MOLDS.each do |name, rules|
+      wax_class = Class.new(String) do |klass|
+        klass::CONSTRUCT = Base64.decode64 MOLDS[name].first
+        (klass::PARTS = (p  = MOLDS[name].last) && (p.is_a?(Hash) ? p : { body: p })).each do |part, pattern|
+          define_method part.to_sym do
+            tap { @part = part }
+          end
         end
+
+        def initialize
+          super self.class::CONSTRUCT
+        end
+
+        def <<(wax)
+          Array(self.class::PARTS[@part || :body]).each { |pattern| sub! pattern, "\\1\\2#{Array(wax).shift}\\3" }
+          tap { @part = nil }
+        end
+
       end
 
-      # Mmmh... What ?
-      def parent_link(path)
-        link 'browse', path.dirname, 'Parent directory'
+      define_method name.to_s.downcase.to_sym do |*args, &block|
+        (wax_class.new << args).tap { |m| block.call(m) unless block.nil? }
       end
 
-      # link depending on what's to be linked
-      def cell_link(cell)
-        send cell.linker, cell
-      end
-
-      # Mmmh... Er ?
-      def show_link(path)
-        link 'show', path, path.basename
-      end
-
-      # Mmmh... Er what ?
-      def browse_link(path)
-        link 'browse', path, path.basename
-      end
-
+      const_set name, wax_class
     end
 
   end
