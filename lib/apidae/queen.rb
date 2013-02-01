862de4328eeb6f192fd32e66120343cda30a3a7d
lacravate <david.elbaz@af83.com>
2013-02-01 02:17:16 +0100

    executable; Hive implantation (run) is done with the help of a Queen

---
+++ lib/apidae/queen.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+# Minimal CLI
+require 'getopt/long'
+
+module Apidae
+
+  module Queen
+
+    attr_writer :charmer
+
+    def implant!
+      # Need help ?
+      if ['help', 'h'].any? { |o| environment.has_key? o }
+        puts etiquette
+        exit
+      else
+        # set the root directory, current directory as default, and go!
+        settings.location = environment.delete('browse') { '.' }
+        run! environment
+      end
+    end
+
+    def environment
+      @environment ||= Getopt::Long.getopts(
+        ['--help', Getopt::OPTIONAL],
+        ['--server', Getopt::OPTIONAL],
+        ['--port', Getopt::OPTIONAL],
+        ['--daemonize', Getopt::OPTIONAL],
+        ['--browse', Getopt::OPTIONAL]
+      )
+    end
+
+    def etiquette
+      "Usage : #{charmer} [--server <server_name> --port <port_number> --daemonize --browse </path/to/browse>]"
+    end
+
+    def charmer
+      @charmer ||= self.name.split('::').first.downcase
+    end
+
+  end
+
+end
+
