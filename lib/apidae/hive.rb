34c985dd03ba96bdf7bf25007b8269545e0713db
lacravate <david.elbaz@af83.com>
2013-02-05 02:48:04 +0100

    Hive; Swarm; code base is now the Swarm; Hive is the application;

--- lib/apidae/hive.rb
+++ lib/apidae/hive.rb
@@ -1,69 +1,9 @@
 # encoding: utf-8
 
-# the ubiquitous delegator
-require 'forwardable'
+# application code base
+require 'apidae/swarm'
 
-# Sinatra, the actual star of the show
-require 'sinatra/base'
-
-# Sinatra routes from configuration
-require 'ways-and-means'
-
-module Apidae
-
-  # the app'. Thanks to "Mr Blue Eyes" talent
-  class Swarm < Sinatra::Base
-
-    class << self
-
-      attr_accessor :location, :current
-
-      def implant!
-        require 'apidae/queen'
-        super
-      end
-
-      # routes and minimal config
-      def ways_and_location
-        { ways: [ 'browse', 'browse/*', 'show/*', 'read/*' ], means: { location: location } }
-      end
-
-      private
-
-    end
-
-    extend Forwardable
-    register Sinatra::WaysAndMeans
-
-    ways_and_means! ways_and_location
-
-    # "settings" is not pretty. And it's less typing. So...
-    def_delegators :settings, :current
-    def_delegators :location, :branching, :read
-
-    attr_accessor :location
-
-    def initialize
-      super
-
-      @location = settings.root_class.new(settings.location).tap do |l|
-        l.branching_class = settings.branching_class if settings.branching_class
-        l.absolute!
-      end
-    end
-
-    private
-
-    # before hook, thanks to ways-and-means, to have the current path available
-    # everywhere as `current`
-    def before_anyway
-      settings.current = location.select (params.any? && params['splat'].first) || ''
-    end
-
-  end
-
-end
-
+# specific requirement for Apidae::Hive
 require 'apidae/worker'
 require 'apidae/cell'
 require 'pathstring_root'
@@ -76,7 +16,7 @@ module Apidae
     set :root_class, PathstringRoot
     set :branching_class, Apidae::Cell
 
-    # routes callbacks provider
+    # Hive population, routes callbacks provider
     include Apidae::Worker
 
   end
---
+++ lib/apidae/swarm.rb
@@ -0,0 +1,66 @@
+# encoding: utf-8
+
+# the ubiquitous delegator
+require 'forwardable'
+
+# Sinatra, the actual star of the show
+require 'sinatra/base'
+
+# Sinatra routes from configuration
+require 'ways-and-means'
+
+module Apidae
+
+  # the app'. Thanks to "Mr Blue Eyes" talent
+  class Swarm < Sinatra::Base
+
+    class << self
+
+      attr_accessor :location, :current
+
+      def implant!
+        require 'apidae/queen'
+        super
+      end
+
+      # routes and minimal config
+      def ways_and_location
+        { ways: [ 'browse', 'browse/*', 'show/*', 'read/*' ], means: { location: location } }
+      end
+
+      private
+
+    end
+
+    extend Forwardable
+    register Sinatra::WaysAndMeans
+
+    ways_and_means! ways_and_location
+
+    # "settings" is not pretty. And it's less typing. So...
+    def_delegators :settings, :current
+    def_delegators :location, :branching, :read
+
+    attr_accessor :location
+
+    def initialize
+      super
+
+      @location = settings.root_class.new(settings.location).tap do |l|
+        l.branching_class = settings.branching_class if settings.branching_class
+        l.absolute!
+      end
+    end
+
+    private
+
+    # before hook, thanks to ways-and-means, to have the current path available
+    # everywhere as `current`
+    def before_anyway
+      settings.current = location.select (params.any? && params['splat'].first) || ''
+    end
+
+  end
+
+end
+
